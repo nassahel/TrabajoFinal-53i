0c3016932b8f4254829a8165ea40def8030e733a
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-08-13 01:00:31 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/navbar'
+
+function App() {
+ 
+return (
+  <div>
+    <Navbar/>
+    <Footer/>
+  </div>
+)
+
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="https://png.pngtree.com/png-clipart/20200727/original/pngtree-restaurant-logo-design-vector-template-png-image_5441058.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
+      Bootstrap
+    </a>
+    <form class="d-flex col-5" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-primary" type="submit">Search</button>
+      </form>
+      <div>
+      <button type="button" class="btn btn-link">Entrar</button>
+      <button type="button" class="btn btn-link">Registrarse</button>
+      </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
