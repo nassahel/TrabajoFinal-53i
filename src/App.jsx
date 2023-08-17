e1960505d3b7379998abfde7a2265f83083043ea
luischehin <luischehinok@gmail.com>
2023-08-17 12:19:23 -0300

    Cree el modal del login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Aside from './components/aside/Aside'
 import Footer from './components/footer/Footer'
 import Main from './components/main/Main'
-
-import { Form } from './components/form/form'
 import Navbar from './components/navbar/Navbar'
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
     <div>
       <Navbar />
       <div class="container-fluid d-flex">
-        <Form />
         <Aside />
         <Main />
 
