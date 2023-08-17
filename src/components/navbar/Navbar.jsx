0d067a1219c3422099486a467de8a8b9e5b25e32
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-08-17 12:31:39 -0300

    se cambio estilos al nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
+import "./navbar.css"
 
 function Navbar() {
   return (
-    <div>
-        <nav class="navbar bg-body-tertiary shadow mb-5">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-        <img src="https://png.pngtree.com/png-clipart/20200727/original/pngtree-restaurant-logo-design-vector-template-png-image_5441058.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
-      Gastronova
-    </a>
-    <form class="d-flex col-5" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-primary" type="submit">Buscar</button>
-      </form>
-      <div>
-      <button type="button" class="btn btn-link">Entrar</button>
-      <button type="button" class="btn btn-link">Registrarse</button>
-      </div>
-  </div>
-</nav>
+    <div class="sticky-top">
+      <nav class="navbar nav_color  shadow mb-5">
+        <div class="container-fluid">
+          <a class="navbar-brand fs-3" href="#">
+            <img src="https://png.pngtree.com/png-clipart/20200727/original/pngtree-restaurant-logo-design-vector-template-png-image_5441058.jpg" alt="Logo" width="40" class="d-inline-block align-text-top me-2" />
+            Santorini
+          </a>
+          <form class="d-flex col-5" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-light" type="submit">Buscar</button>
+          </form>
+          <div>
+            <button type="button" class="btn btn-light me-2">Entrar</button>
+            <button type="button" class="btn btn-light">Registrarse</button>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
