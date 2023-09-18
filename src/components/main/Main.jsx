07bbd7eb8d8bbfbe3fe848699f992336089cd5a3
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-09-18 20:12:02 -0300

    se establecio imagen de fondo y se corrigio spinner

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,44 +1,31 @@
 import { useState, useEffect } from 'react'
 import React from 'react'
 import './mains.css'
-import SearchBar from '../searchBar/SearchBar'
 import Spiner from '../spiner/Spiner'
 
-function Main() {
+function Main({ products, searchTerm, loading }) {
 
-    const [products, setProducts] = useState([])
-    const [loading, setLoading] = useState(false)
+    
+    const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(searchTerm.toLowerCase()))
 
-    const productsStore = async () => {
-    setLoading(true)
-        const data = await fetch('https://fakestoreapi.com/products');
-        const prom = await data.json();
-        setProducts(prom)
-        setLoading(false)
-    }
 
-    useEffect(() => {
-        productsStore()
-    }, [])
-
-    console.log(products);
 
     return (
         <div className='container-fluid py-4'>
-            <SearchBar />
             <div className='row gap-5 justify-content-center product-list'>
-                <h2 className='text-center mt-3'>Categoria</h2>
+                <h2 className='text-center mt-3 text-light'>Productos</h2>
 
-                {loading ? (<Spiner/>) :  products.map((prod) => (
-                    <div className="card col-lg-2 col-md-4 shadow align-items-center justify-content-center">
+                { loading ? <Spiner/> : filteredProducts.map((product) => (
+                    <div className="card col-lg-2 col-md-4 shadow align-items-center justify-content-center" key={product.id}>
                         <div className="image-div d-flex align-items-center justify-content-center">
-                            <img className='img-fluid h-100' src={prod.image} alt={prod.title} />
+                            <img className='img-fluid h-100' src={product.image} alt={product.title} />
                         </div>
                         <div className="title-div d-flex align-items-center">
-                            <h5 className='text-center text-justify '>{prod.title} </h5>
+                            <h5 className='text-center text-justify '>{product.title} </h5>
                         </div>
                         <div className="product-div">
-                            <h5 className='text-primary border'>${prod.price}</h5>
+                            <h5 className='text-primary border'>${product.price}</h5>
                         </div>
                         <div className="button-div">
                             <button className='btn btn-outline-warning fw-bold'>Comprar</button>
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,77 +1,39 @@
 import React from 'react'
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import './searchBar.css'
 
-const URL = 'https://fakestoreapi.com/products';
 
-function SearchBar() {
-  const [search, setSearch] = useState('');
-  const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setProducts(data);
-      } else {
-        throw new Error('Error fetching data');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
+function SearchBar({ setSearchTerm }) {
+ 
+  const [query, setQuery] = useState('');
 
   const handleSearch = () => {
-    const filtered = products.filter((product) =>
-      product.title.toLowerCase().includes(search.toLowerCase())
-    );
-    setFilteredProducts(filtered);
-  };
+    setSearchTerm(query);
+  }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
 
   return (
     <div className='container-fluid flex-column d-flex justify-content-center align-items-center'>
       <Form className="containter-fluid d-flex col-lg-5 col-12 my-4">
-        <Form.Control onChange={(e) => setSearch(e.target.value)} value={search}
+        <Form.Control onChange={(e) => setQuery(e.target.value)} value={query} onKeyDown={handleKeyPress}
           type="search"
           placeholder="Buscar..."
           className="me-2 form-control-lg"
           aria-label="Search"
           data-bs-theme="light"
-        />
-        <Button className='bg-dark btn btn-md' onClick={handleSearch} variant="outline-light"><BsSearch size='2rem' /></Button>
+        /> 
+        <Button className='bg-dark btn btn-md' onKeyDown={handleKeyPress} onClick={handleSearch}  variant="outline-light"><BsSearch size='2rem' /></Button>
       </Form>
-
-      <div className='container-fluid d-flex gap-3 flex-wrap'>
-        {filteredProducts.map((prod) => (
-
-
-          <div className="card col-lg-2 col-md-4 shadow align-items-center justify-content-center">
-            <div className="image-div d-flex align-items-center justify-content-center">
-              <img className='img-fluid h-100' src={prod.image} alt={prod.title} />
-            </div>
-            <div className="title-div d-flex align-items-center">
-              <h5 className='text-center text-justify '>{prod.title} </h5>
-            </div>
-            <div className="product-div">
-              <h5 className='text-primary border'>${prod.price}</h5>
-            </div>
-            <div className="button-div">
-              <button className='btn btn-outline-warning fw-bold'>Comprar</button>
-            </div>
-
-          </div>
-
-        ))}
-      </div>
     </div>
   )
 }
