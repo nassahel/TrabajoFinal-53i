c28f497263a19d049ced65e2c8636796002bd81c
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-09-25 15:53:58 -0300

    se modificaron detalles del footer

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,24 +2,90 @@ import { useState, useEffect } from 'react'
 import React from 'react'
 import './mains.css'
 import Spiner from '../spiner/Spiner'
+import comidas from './comidas'
 
 function Main({ products, searchTerm, loading }) {
 
-    const filteredProducts = products.filter((product) =>
-        product.title.toLowerCase().includes(searchTerm.toLowerCase()))
+    const filteredProducts = products.filter((product) =>        //cuando tenga la api abajo en vez de comidas tengo que poner la variable esta
+        product.title.toLowerCase().includes(searchTerm.toLowerCase())) 
+
+
+
+      const catEntradas = comidas.filter((comi) => comi.categoria == 'Entradas' )
+      const catPizzas = comidas.filter((comi) => comi.categoria == 'Pizzas' )
+      const catCarnes = comidas.filter((comi) => comi.categoria == 'Carnes' )
+      const catBebidas = comidas.filter((comi) => comi.categoria == 'Bebidas' )
+
 
     return (
         <div className='container'>
-            <h2 className='text-center mt-3 text-light bg-warning py-2 bg-opacity-75'>Productos</h2>
+            <h2 className='text-center mt-3 bg-warning py-3'>Entradas</h2>
             <div className='row row-cols-lg-4 row-cols-1 row-cols-md-2 g-5 my-5'>
-                {loading ? <Spiner /> : filteredProducts.map((product) => (
+                {loading ? <Spiner /> : catEntradas.map((product) => (
 
                     <div className="col col-lg-3">
-                        <div className="card text-center border-4 border-dark p-3 h-100" key={product.id}>
-                            <img className='card-img-top w-50 mx-auto' src={product.image} alt={product.title} />
+                        <div className="card text-center border-2 border-dark p-3 h-100" key={product.id}>
+                            <img className='card-img-top w-50 mx-auto' src={product.imagen} alt={product.nombre} />
                             <div className="card-body d-flex flex-column justify-content-end">
-                                <h5 className='card-title'>{product.title} </h5>
-                                <h5 className='card-title'>${product.price}</h5>
+                                <h5 className='card-title'>{product.nombre} </h5>
+                                <h5 className='card-title'>${product.precio}</h5>
+                                <button className='btn btn-outline-warning rounded-0 fw-bold'>Comprar</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                ))}
+            </div>
+
+            <h2 className='text-center mt-3 bg-warning py-3'>Pizzas</h2>
+            <div className='row row-cols-lg-4 row-cols-1 row-cols-md-2 g-5 my-5'>
+                {loading ? <Spiner /> : catPizzas.map((product) => (
+
+                    <div className="col col-lg-3">
+                        <div className="card text-center border-2 border-dark p-3 h-100" key={product.id}>
+                            <img className='card-img-top w-50 mx-auto' src={product.imagen} alt={product.nombre} />
+                            <div className="card-body d-flex flex-column justify-content-end">
+                                <h5 className='card-title'>{product.nombre} </h5>
+                                <h5 className='card-title'>${product.precio}</h5>
+                                <button className='btn btn-outline-warning rounded-0 fw-bold'>Comprar</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                ))}
+            </div>
+
+            <h2 className='text-center mt-3 bg-warning py-3'>Carnes</h2>
+            <div className='row row-cols-lg-4 row-cols-1 row-cols-md-2 g-5 my-5'>
+                {loading ? <Spiner /> : catCarnes.map((product) => (
+
+                    <div className="col col-lg-3">
+                        <div className="card text-center border-2 border-dark p-3 h-100" key={product.id}>
+                            <img className='card-img-top w-50 mx-auto' src={product.imagen} alt={product.nombre} />
+                            <div className="card-body d-flex flex-column justify-content-end">
+                                <h5 className='card-title'>{product.nombre} </h5>
+                                <h5 className='card-title'>${product.precio}</h5>
+                                <button className='btn btn-outline-warning rounded-0 fw-bold'>Comprar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                ))}
+            </div>
+
+            <h2 className='text-center mt-3 bg-warning py-3'>Bebidas</h2>
+
+            <div className='row row-cols-lg-4 row-cols-1 row-cols-md-2 g-5 my-5'>
+                {loading ? <Spiner /> : catBebidas.map((product) => (
+
+                    <div className="col col-lg-3">
+                        <div className="card text-center border-2 border-dark p-3 h-100" key={product.id}>
+                            <img className='card-img-top w-50 mx-auto' src={product.imagen} alt={product.nombre} />
+                            <div className="card-body d-flex flex-column justify-content-end">
+                                <h5 className='card-title'>{product.nombre} </h5>
+                                <h5 className='card-title'>${product.precio}</h5>
                                 <button className='btn btn-outline-warning rounded-0 fw-bold'>Comprar</button>
                             </div>
                         </div>
