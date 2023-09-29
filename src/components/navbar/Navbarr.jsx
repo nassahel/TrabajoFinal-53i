37d1c6bc4eca7beb52cf840ae583dd8238f9d11c
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-09-29 19:42:24 -0300

    se cambio canvas por drop en navbar

--- src/components/navbar/Navbarr.jsx
+++ src/components/navbar/Navbarr.jsx
@@ -4,10 +4,8 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { BsPersonCircle } from "react-icons/bs";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import Offcanvas from 'react-bootstrap/Offcanvas';
 import logoResto from '../../assets/img/hnb.png'
 import "./navbar.css";
 
@@ -44,48 +42,33 @@ function Navbarr() {
   };
 
   return (
-    <>
-      <Navbar collapseOnSelect key="lg" expand="lg" className="classnav sticky-top shadow p-lg-3 fs-5 mb-3" data-bs-theme="dark">
-        <Container fluid>
-          <NavLink to="/">
-            <img src={logoResto} width="260" className="d-inline-block align-center" alt="React Bootstrap logo" />
-          </NavLink>
-          <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg classnav" />
-          <Navbar.Offcanvas
-            id="offcanvasNavbar-expand-lg"
-            aria-labelledby="offcanvasNavbarLabel-expand-lg"
-            placement="end"
-            data-bs-theme="dark"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
-                Menú
-              </Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-4 mb-2">
-                <NavLink to="/" className="nav-link text-light" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Inicio</NavLink>
-                <NavLink to="/about" className="nav-link text-light" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Nosotros</NavLink>
-                <NavLink to="/admin" className="nav-link text-light" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Admin</NavLink>
-                {cerrarSesion && (
-                  <>
-                    <button onClick={handleLogout}>Cerrar Sesión</button>
-                    {/* Otros enlaces para usuarios autenticados */}
-                  </>
-                )}
-              </Nav>
-              <NavLink to="/register" className='me-4' >
-                <BsPersonCircle size='2rem' color='white' />
-              </NavLink >
+    <Navbar collapseOnSelect expand="md" className="classnav sticky-top shadow p-lg-3 fs-5 mb-3" data-bs-theme="dark">
+      <Container fluid>
+        <NavLink to="/">
+          <img src={logoResto} width="260" className="d-inline-block align-center" alt="React Bootstrap logo" />
+        </NavLink>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav classnav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="justify-content-end flex-grow-1 pe-4 mb-2">
+            <NavLink to="/" className="nav-link text-light" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Inicio</NavLink>
+            <NavLink to="/about" className="nav-link text-light" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Nosotros</NavLink>
+            <NavLink to="/admin" className="nav-link text-light" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Admin</NavLink>
+           
+          </Nav>
+          <NavLink to="/register" className='me-4' >
+             {cerrarSesion && (
+                <button className="btn btn-outline-light btn-sm" onClick={handleLogout}>Cerrar Sesión</button>
+               )}
+            <BsPersonCircle size='2rem' color='white' />
+          </NavLink >
 
-              <NavLink to="#">
-                <AiOutlineShoppingCart size='2rem' color='white' />
-              </NavLink>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
-    </>
+          <NavLink to="#">
+            <AiOutlineShoppingCart size='2rem' color='white' />
+          </NavLink>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
   );
 }
 
