a758352a159ba6e984db7b867e0ae34a08003b04
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-10-01 17:34:28 -0300

    se mejoro componente order

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,28 +1,44 @@
-import React, { useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Main from "../../components/main/Main";
+import { AiFillPlusCircle, AiFillMinusCircle, AiFillDelete } from "react-icons/ai";
 import { useLocation, useNavigate } from "react-router-dom";
 
 
 function Orders() {
   const location = useLocation()
   const navigate = useNavigate()
-  const [order, setOrder] = useState(null) 
+  const [order, setOrder] = useState(null)
   const [error, setError] = useState(null)
-  useEffect( ()=> {
+  const [prodCount, setProdCount] = useState(0);
+
+  const aumentar = () => {
+    if (prodCount >= 0) {
+      setProdCount(prodCount + 1)
+    }
+  }
+
+  const disminuir = () => {
+    if (prodCount >= 1) {
+      setProdCount(prodCount - 1)
+    }
+  }
+
+
+  useEffect(() => {
     const order = JSON.parse(localStorage.getItem("orders"))
     setOrder(order)
-  },[])
-  
+  }, [])
+
   const confirmar = async () => {
     setError(null) // Sacar los errores para empezar de 0
     const fecha = new Date(Date.now()) // Obtiene la fecha del momento del pedido
 
     const token = localStorage.getItem("token")
-    if (!token){
+    if (!token) {
       setError("No estas registrado papilo")
       return
     }
-   
+
     const total = order.reduce((total, actual) => total + actual.price, 0)
     console.log(total)
     const data = { // Armar el objeto para enviar al API
@@ -37,8 +53,8 @@ function Orders() {
       const peticion = await fetch('https://backend-rolling53i.onrender.com/api/pedidos',
         {
           method: "POST",
-          headers:{
-            "Content-Type":"aplication/json;  charset=UTF-8",
+          headers: {
+            "Content-Type": "aplication/json;  charset=UTF-8",
             "Authentication": `Bearer ${token}`,
           },
           body: JSON.stringify(data),
@@ -49,44 +65,46 @@ function Orders() {
         throw new Error("Error prueba mas tarde")
       }
       localStorage.removeItem("orders")
-      window.dispatchEvent( new Event('storage') )
+      window.dispatchEvent(new Event('storage'))
       navigate("/")
     } catch (e) {
       setError(e.message)
-      
+
     }
   }
- 
+
 
 
 
 
   return (
     <div className="container-fluid main-cont d-flex flex-column align-items-center">
-      <img
-        className="col-11 col-lg-6 pt-4 my-4"
-        src="src\assets\img\Titulo_proyecto_final.jpg"
-        alt=""
-      />
-      <div>
-      <button className='btn btn-outline-warning rounded-0 fw-bold' onClick={confirmar}>Confirmar compra</button>
-      {error && <p className="text-danger">{error}</p>} 
-        {order ? order.map( (item) => (
-          <div className="col col-lg-3 w-100" key={item.id}>
-              <div className="card text-center border-4 border-dark p-3 h-100 w-100 my-3" >
-                  <img className='card-img-top w-50 mx-auto' src={item.image} alt={item.name} />
-                  <div className="card-body d-flex flex-column justify-content-end">
-                    <h5 className="card-title" >{item.name}</h5>
-                      <h5 className='card-title'>{item.category} </h5>
-                      <h5 className='card-title'>${item.price}</h5>
-                  </div>
+      <div className="container bg-success text-light p-2 text-center"><h2>Tu pedido</h2></div>
+
+      <div className="container text-center col-lg-7 py-4">
+        {error && <p className="text-danger">{error}</p>}
+        {order ? order.map((item) => (
+          <div className="row bg-light border border-success rounded m-3 py-3" key={item.id}>
+            <div className="col text-start"><img className='img-fluid w-50 rounded' src={item.image} alt={item.name} /></div>
+            <div className="col my-auto"><h5 className="card-title" >{item.name}</h5></div>
+            <div className="col my-auto"><h5 className='card-title'>${item.price}</h5></div>
+            <div className="col my-auto">
+              <h5>Cantidad</h5>
+              <div>
+                <AiFillMinusCircle onClick={disminuir} size='25' color='green' />
+                <input className="w-25 border mx-2 text-center" type="numbre" name="poductMount" id="proMount" value={prodCount} />
+                <AiFillPlusCircle onClick={aumentar} size='25' color='green' />
               </div>
+            </div>
+            <div className="col my-auto text-end pe-4"> <AiFillDelete size={25} color='brown' /></div>
+
           </div>
         )) : <p className="text-danger">No has seleccionado nada</p>}
-        
+
+        <button className='btn btn-md mt-4 btn-info fw-bold' onClick={confirmar}>Confirmar compra</button>
       </div>
     </div>
   );
 }
 
-export default Orders;
+export default Orders
