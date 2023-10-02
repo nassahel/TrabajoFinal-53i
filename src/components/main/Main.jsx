a25357c335470cf17cc7b37321b2639098f94cc6
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-10-01 23:42:59 -0300

    se mejoro estils de la pagina home

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -17,34 +17,41 @@ function Main({ products, searchTerm, loading }) {
     const catDrink = filteredProducts.filter((comi) => comi.category == 'Bebidas')
 
     return (
-        <div className='container'>
-            <h2 className='text-center mt-3 bg-warning py-3'>Entradas</h2>
-            {catEntrees.length === 0 && !loading ? (
-                <p className='p-3 bg-light bg-opacity-75 rounded'>No se encontró nada en esta sección...</p>
-            ) : (
-                <div className='row row-cols-lg-4 row-cols-1 row-cols-md-2 g-5 my-5'>
-                    {loading ? (
-                        <Spiner />
+        <div className='container-fluid'>
+
+            <div className="green container-fluid col-10 p-3 rounded">
+                <div className="container">
+                    <h2 className='text-center mt-3 text-white display-1 py-3 sacramentoFont'>Entradas</h2>
+                    {catEntrees.length === 0 && !loading ? (
+                        <p className='p-3 bg-light bg-opacity-75 rounded'>No se encontró nada en esta sección...</p>
                     ) : (
-                        catEntrees.map((product, index) => (
-                            <div className="col col-lg-3" key={product.id || index}>
-                                <div className="card text-center border-1 border-dark p-1 h-100">
-                                    <img className='img-fluid mx-auto' src={product.image} alt={product.name} />
-                                    <div className="card-body d-flex flex-column justify-content-end">
-                                        <h5 className='card-title'>{product.name}</h5>
-                                        <h5 className='card-title'>${product.price}</h5>
-                                        <button
-                                            onClick={() => navigate("/user/orders", { state: product })}
-                                            className='btn btn-outline-warning text-dark border-2 rounded-0 fw-bold'>Agregar al carrito</button>
+                        <div className='row row-cols-lg-4 row-cols-1 row-cols-md-2 g-5 mb-5'>
+                            {loading ? (
+                                <Spiner />
+                            ) : (
+                                catEntrees.map((product, index) => (
+                                    <div className="col col-lg-3" key={product.id || index}>
+                                        <div className="card text-center border-1 border-dark p-1 h-100">
+                                            <img className='img-fluid mx-auto' src={product.image} alt={product.name} />
+                                            <div className="card-body d-flex flex-column justify-content-end">
+                                                <h5 className='card-title'>{product.name}</h5>
+                                                <h5 className='card-title'>${product.price}</h5>
+                                                <button
+                                                    onClick={() => navigate("/user/orders", { state: product })}
+                                                    className='btn btn-outline-success text-dark border-2 rounded-0 fw-bold'>Agregar al carrito</button>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                        ))
+                                ))
+                            )}
+                        </div>
                     )}
                 </div>
-            )}
 
-<h2 className='text-center mt-3 bg-warning py-3'>Pizzas</h2>
+
+            </div>
+
+            <h2 className='text-center mt-3  py-3 green text-white sacramentoFont'>Pizzas</h2>
             {catPizzas.length === 0 && !loading ? (
                 <p className='p-3 bg-light bg-opacity-75 rounded'>No se encontró nada en esta sección...</p>
             ) : (
@@ -61,7 +68,7 @@ function Main({ products, searchTerm, loading }) {
                                         <h5 className='card-title'>${product.price}</h5>
                                         <button
                                             onClick={() => navigate("/user/orders", { state: product })}
-                                            className='btn btn-outline-warning text-dark border-2 rounded-0 fw-bold'>Agregar al carrito</button>
+                                            className='btn btn-outline-success text-dark border-2 rounded-0 fw-bold'>Agregar al carrito</button>
                                     </div>
                                 </div>
                             </div>
@@ -70,9 +77,9 @@ function Main({ products, searchTerm, loading }) {
                 </div>
             )}
 
-<h2 className='text-center mt-3 bg-warning py-3'>Carnes</h2>
+            <h2 className='text-center mt-3 py-3 text-white sacramentoFont'>Carnes</h2>
             {catMeat.length === 0 && !loading ? (
-                <p className='p-3 bg-light bg-opacity-75 rounded'>No se encontró nada en esta sección...</p>
+                <p className='p-3 bg-light bg-opacity-75 rounded green'>No se encontró nada en esta sección...</p>
             ) : (
                 <div className='row row-cols-lg-4 row-cols-1 row-cols-md-2 g-5 my-5'>
                     {loading ? (
@@ -87,7 +94,7 @@ function Main({ products, searchTerm, loading }) {
                                         <h5 className='card-title'>${product.price}</h5>
                                         <button
                                             onClick={() => navigate("/user/orders", { state: product })}
-                                            className='btn btn-outline-warning text-dark border-2 rounded-0 fw-bold'>Agregar al carrito</button>
+                                            className='btn btn-outline-success text-dark border-2 rounded-0 fw-bold'>Agregar al carrito</button>
                                     </div>
                                 </div>
                             </div>
@@ -96,7 +103,7 @@ function Main({ products, searchTerm, loading }) {
                 </div>
             )}
 
-<h2 className='text-center mt-3 bg-warning py-3'>Bebidas</h2>
+            <h2 className='text-center mt-3  py-3 green text-white sacramentoFont'>Bebidas</h2>
             {catDrink.length === 0 && !loading ? (
                 <p className='p-3 bg-light bg-opacity-75 rounded'>No se encontró nada en esta sección...</p>
             ) : (
@@ -113,7 +120,7 @@ function Main({ products, searchTerm, loading }) {
                                         <h5 className='card-title'>${product.price}</h5>
                                         <button
                                             onClick={() => navigate("/user/orders", { state: product })}
-                                            className='btn btn-outline-warning text-dark border-2 rounded-0 fw-bold'>Agregar al carrito</button>
+                                            className='btn btn-outline-success text-dark border-2 rounded-0 fw-bold'>Agregar al carrito</button>
                                     </div>
                                 </div>
                             </div>
