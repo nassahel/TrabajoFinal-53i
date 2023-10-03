a45ef512e12642a47048b53efc3e2ef1655742db
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-10-02 22:03:25 -0300

    se corrigio un error del logueo

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './login.css';
